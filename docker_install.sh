bf00cfba64a69bdd859ae2909b73a5ef81e0d70c
kushal <kushaldeveloper@gmail.com>
2017-01-15 18:37:54 +0000

    add apt-get install apt-transport-https and yqq
    https://askubuntu.com/a/104185/14784
    
    Signed-off-by: kushal <kushaldeveloper@gmail.com>

--- docker_install.sh
+++ docker_install.sh
@@ -8,5 +8,6 @@ apt-get install git -yqq
 #install dotnet 
 sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
-apt-get update
-apt-get install dotnet-dev-1.0.0-preview2.1-003177
+apt-get install apt-transport-https -yqq
+apt-get update -yqq
+apt-get install dotnet-dev-1.0.0-preview2.1-003177 -yqq
